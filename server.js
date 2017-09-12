767d22099b9c9ca6c8bd2fdfec9fa8925b24e6f1
stiekel <histkc@gmail.com>
2017-09-13 07:23:40 +0800

    add connection and disconnect event handler on server side

--- server.js
+++ server.js
@@ -4,5 +4,12 @@ const io = require('socket.io')(server, {
   path: '/'
 })
 
+io.on('connection', (socket) => {
+  console.log(socket.conn.remoteAddress, 'connected')
+  socket.on('disconnect', (reason) => {
+    console.log(socket.conn.remoteAddress, 'disconnect, reason:', reason)
+  })
+})
+
 server.listen(8081)
 console.log('socket.io listen on 8081')
